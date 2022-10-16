06f7a890686da2dcada4e3aa932562b000ee6502
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-10-16 16:35:18 -0600

    heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,10 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
-const { request } = require("express");
+// const { request } = require("express");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -30,7 +30,7 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
+  new CacheFirst({
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,12 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log("Updated Database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("Data has been saved to Database.", result);
 };
@@ -29,10 +29,10 @@ export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log("GET result from database:", result);
-  return result;
+  return result?.value;
 };
 
 initdb();
